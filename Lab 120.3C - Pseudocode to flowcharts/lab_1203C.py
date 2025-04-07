b0985452330449eb1e63ee03b9d294feb2beb02c
simhi.boaz <simhi.boaz@emanuelschool.nsw.edu.au>
2025-04-07 12:08:45 +1000

    Almost done lab_1203C

--- Lab 120.3C - Pseudocode to flowcharts/lab_1203C.py
+++ Lab 120.3C - Pseudocode to flowcharts/lab_1203C.py
@@ -4,12 +4,16 @@ n = int(input("Input a number: "))
 if n < 2:
     print("Not Prime")
 else:
+    print("ceiling root is", ceil(sqrt(n)))
     prime = True
-    for i in range(2, ceil(sqrt(n))):
+    for i in range(2, ceil(sqrt(n)) + 1):
+        print(i)
         if n % i == 0:
             prime = False
+            print("mustn't be prime")
             break
-    if prime:
-        print("Prime")
-    else:
-        print("Not Prime")
+
+if prime:
+    print("Prime")
+else:
+    print("Not Prime")
